e3d798ccc77d2271caf5979f3ef49e27b60f5e5d
Muhammad Soman <somanamir43@gmail.com>
2025-03-21 02:13:50 +0500

    add a new file that is palindrome number

---
+++ BasicMathForJava/Armstrong.java
@@ -0,0 +1,29 @@
+package BasicMathForJava;
+
+import java.util.Scanner;
+
+public class Armstrong {
+    public static void main(String[] args) {
+        
+		  Scanner sc = new Scanner(System.in);
+	        System.out.print("Enter the number: ");
+	        int n = sc.nextInt();
+	        sc.close();
+	        
+	        int reversed=0;
+	        int dup=n;
+	        while(n>0) {
+	        	  int digit = n % 10;  // Extract last digit
+	              reversed = reversed * 10 + digit; // Append digit to reversed number
+	              n = n / 10;
+	           
+	        }
+	        if(reversed==dup) {
+          	  System.out.println("true the number you enter not a palindrome");
+            }
+            else {
+          	  System.out.println("False");
+            }
+		
+    }
+}
